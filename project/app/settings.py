f3b0c0395902bcbb105f3f1a664a414597486d97
Levon Baghdasaryan <levonbaghdasarian@gmail.com>
2023-10-06 23:05:19 +0400

    pytest added

--- project/app/settings.py
+++ project/app/settings.py
@@ -40,6 +40,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # External Packages
+    'rest_framework',
+
+    # Internal Apps
+
 ]
 
 MIDDLEWARE = [
