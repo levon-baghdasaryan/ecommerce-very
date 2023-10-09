9897bc9607abbc66c1e1dd94d625e039894fd2e6
Levon Baghdasaryan <levonbaghdasarian@gmail.com>
2023-10-09 16:12:48 +0400

    category viewset

--- project/product/views.py
+++ project/product/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Category
+from .serializers import CategorySerializer
+
+
+class CategoryViewSet(viewsets.ViewSet):
+    """
+    A simple Viewset for viewing all categories
+    """
+
+    def list(self, request):
+        queryset = Category.objects.all()
+        serializer = CategorySerializer(queryset, many=True)
+        return Response(serializer.data)
