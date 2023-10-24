6f793761de8a37054f8f382d27c834c1d7eceb4d
john-doe <john@example.com>
2023-10-24 17:19:00 +0400

    product line added to product serializer

--- project/product/serializers.py
+++ project/product/serializers.py
@@ -6,27 +6,26 @@ from .models import Category, Brand, Product, ProductLine
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = "__all__"
+        fields = ["name"]
 
 
 class BrandSerializer(serializers.ModelSerializer):
     class Meta:
         model = Brand
+        exclude = ("id",)
+
+
+class ProductLineSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductLine
         fields = "__all__"
 
 
 class ProductSerializer(serializers.ModelSerializer):
     brand = BrandSerializer()
     category = CategorySerializer()
+    product_line = ProductLineSerializer(many=True)
 
     class Meta:
         model = Product
-        fields = "__all__"
-
-
-class ProductLineSerializer(serializers.ModelSerializer):
-    product = ProductSerializer()
-
-    class Meta:
-        model = ProductLine
-        fields = "__all__"
+        exclude = ("id",)
